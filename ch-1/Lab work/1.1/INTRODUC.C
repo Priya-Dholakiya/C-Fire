796788cf89822c15ad9d6d08bb2f33d9f1655953
Priya-Dholakiya <priyadholakiya16@gmail.com>
2024-08-13 10:11:35 +0530

    13/08/2024

---
+++ ch-1/Lab work/1.1/INTRODUC.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+   clrscr();
+
+	   printf("Name\t: Priya Dholakiya\n");
+	   printf("Age\t: 17\n");
+	   printf("School\t: Aspire Public School");
+
+   getch();
+
+}
---
+++ ch-1/Lab work/1.1/SQUARE.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+      float C =30.0;
+      float F;
+
+      F=(9/5)*C+32;
+
+   clrscr();
+
+	    printf("%2.f degree celcius is equal to %.2f degree Fahrenheit\n",C,F);
+
+   getch();
+
+}
