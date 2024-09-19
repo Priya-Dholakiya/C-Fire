f71ed005d97000fe434e28ec2fea1223558e7b4e
Priya Dholakiya <priyadholakiya16@gmail.com>
2024-09-19 21:46:33 +0530

    Add files via upload

---
+++ With Space Pattern/38.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+	int i,j,s;
+
+	clrscr();
+		for(i=65;i<=69;i++)
+		{
+		   for(s=65;s<i;s++)
+		   {
+			printf(" ");
+		   }
+		   for(j=i;j<=69;j++)
+		   {
+			printf("%c",j);
+		   }
+			printf("\n");
+		}
+
+	getch();
+
+}
